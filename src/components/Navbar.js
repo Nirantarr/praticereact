4da080fed8d8ccab1f201a27b53f1691974295b7
Nirantarr <114576345+Nirantarr@users.noreply.github.com>
2023-01-24 13:46:23 +0530

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,29 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 export default function Navbar(props) {
+
   return (
-    <nav className={`navbar navbar-expand-lg -${props.mode} bg-${props.mode}`}>
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/"></a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">{props.Home}</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/about">About</a>
-          </li>
-         
-          {/* <li className="nav-item">
-            <a className="nav-link disabled">Disabled</a>
-          </li> */}
-        </ul>
-        <div className={`form-check form-switch  -${props.mode==='white'?'dark':'white'}`}>
-  <input className="form-check-input" onClick={props.ChangeScreenMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-  <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >Enable Dark Mode</label>
-</div>
+    <nav className={`navbar navbar-expand-lg -${props.mode} bg-${props.mode} border border-3`}>
+      <div className="container-fluid ">
+        <Link className="navbar-brand" style={{ color: props.mode === 'white' ? 'black' : 'white' }} to="">TextUtils</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item"  >
+              <Link className="nav-link active" style={{ color: props.mode === 'white' ? 'black' : 'white' }} aria-current="page" to="/">{props.Home}</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" style={{ color: props.mode === 'white' ? 'black' : 'white' }} to="/about">About</Link>
+            </li>
+          </ul>
+          <div className={`form-check form-switch  `}>
+            <input className="form-check-input" onClick={props.ChangeScreenMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label className="form-check-label" style={{ color: props.mode === 'white' ? 'black' : 'white' }} htmlFor="flexSwitchCheckDefault" >{props.BtnText}</label>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
-Navbar.defaultProps={
-Home: 'Set Title here'
-};
-
